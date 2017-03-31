002ce2ccc8f1c3be2fe1ac8a2485897f4b89b845
Adrià Gironès <adriagirones@gmail.com>
2017-03-31 20:19:24 +0200

    SDL6

--- SDL6_Handout/ModuleSceneIntro.cpp
+++ SDL6_Handout/ModuleSceneIntro.cpp
@@ -35,9 +35,11 @@ bool ModuleSceneIntro::Start()
 		else
 		{
 			Mix_Init(MIX_INIT_OGG);
-			Mix_OpenAudio(44000, MIX_DEFAULT_FORMAT, 2, 2048);
+			Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
 			music = Mix_LoadMUS("rtype/intro.ogg");
 			Mix_PlayMusic(music, -1);
+
+			starting = Mix_LoadWAV("rtype/starting.wav");
 		}
 
 	return true;
@@ -46,10 +48,10 @@ bool ModuleSceneIntro::Start()
 // UnLoad assets
 bool ModuleSceneIntro::CleanUp()
 {
-
-
 	LOG("Unloading SDL audio");
 	Mix_FreeMusic(music);
+	Mix_FreeChunk(starting);
+	Mix_CloseAudio();
 
 	LOG("Unloading space scene");
 
@@ -66,21 +68,8 @@ update_status ModuleSceneIntro::Update()
 
 	if (App->input->keyboard[SDL_SCANCODE_SPACE] == 1) {
 		App->fade->FadeToBlack(this, App->scene_space, 2);
-		LOG("Init SDL audio");
 
-		if (SDL_Init(SDL_INIT_AUDIO) < 0)
-		{
-			LOG("SDL_AUDIO could not initialize! SDL_Error:\n");
-			LOG(SDL_GetError());
-		}
-		else
-		{
-			Mix_Init(MIX_INIT_MP3);
-			Mix_OpenAudio(44000, MIX_DEFAULT_FORMAT, 2, 2048);
-			music = Mix_LoadMUS("rtype/starting.wav");
-			Mix_PlayMusic(music, -1);
-		}
-		Mix_FreeChunk(starting);
+		Mix_PlayChannel(-1, starting, 0);
 }
 
 	return UPDATE_CONTINUE;
